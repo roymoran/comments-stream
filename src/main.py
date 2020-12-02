4ed084a5e51c23c6e28fbf147969efe2787c46fe
Roy Moran <romoran1@outlook.com>
2020-12-02 02:09:49 -0600

    print comment

--- src/main.py
+++ src/main.py
@@ -5,11 +5,15 @@ APP_VERSION = 'v0.1.0'
 reddit = praw.Reddit(client_id=os.environ['REDDIT_CLIENT_ID'],
                      client_secret=os.environ['REDDIT_CLIENT_SECRET'], user_agent=f'python3.8:{APP_VERSION} (by /u/bookfinderbot)')
 subreddit = 'all'
-
+comments = []
 
 def stream():
     for comment in reddit.subreddit(subreddit).stream.comments():
-        print(comment)
+        print("-------------New Comment--------------")
+        print(comment.body)
+        comments.append(comment.body)
+        print(len(comments))
+    
 
 
 stream()
