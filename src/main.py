2c61f325d3c760622495a94d1decb9be6d173367
Roy Moran <romoran1@outlook.com>
2020-12-02 07:21:43 -0600

    remove comments list

--- src/main.py
+++ src/main.py
@@ -5,15 +5,10 @@ APP_VERSION = 'v0.1.0'
 reddit = praw.Reddit(client_id=os.environ['REDDIT_CLIENT_ID'],
                      client_secret=os.environ['REDDIT_CLIENT_SECRET'], user_agent=f'python3.8:{APP_VERSION} (by /u/bookfinderbot)')
 subreddit = 'all'
-comments = []
 
 def stream():
     for comment in reddit.subreddit(subreddit).stream.comments():
         print("-------------New Comment--------------")
         print(comment.body)
-        comments.append(comment.body)
-        print(len(comments))
-    
-
 
 stream()
