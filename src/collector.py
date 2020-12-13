09b3fb3efd8614763992d38ec0c04a61c6ddf20f
Roy Moran <romoran1@outlook.com>
2020-12-13 14:14:57 -0600

    allow selection of keyword combinations to search

--- src/collector.py
+++ src/collector.py
@@ -2,21 +2,39 @@ import os
 import sys
 import time
 from pathlib import Path
+from itertools import chain, combinations
 from src.stream import stream_generator
 
 training_set_files_count = 0
 testing_set_files_count = 0
+keyword_powerset = set()
 
 
 def collector():
     global testing_set_files_count
     global training_set_files_count
+
+    global keyword_powerset
+
     if len(sys.argv) <= 1:
         print('no keyword arguments provided.')
         return
 
     print(f'\nRunning collector with keywords: {", ".join(sys.argv[1:])}\n')
-    keyword_set = set(sys.argv[1:])
+
+    keyword_powerset = sorted(
+        set(powerset(sys.argv[1:])), key=len, reverse=True)
+    keyword_search_set = set()
+
+    if len(keyword_powerset) > 1:
+        print('Select keyword combinations to include in search')
+        for ks in keyword_powerset:
+            response = input(
+                f'Include {ks}? [Y/y or any key to exclude]. ')
+            if response.lower() == 'y':
+                keyword_search_set.add(ks)
+    else:
+        keyword_search_set = keyword_powerset
 
     training_set_files_count = training_set_count()
     testing_set_files_count = testing_set_count()
@@ -27,12 +45,18 @@ def collector():
         for comment in stream:
             print(f'{training_set_files_count} file(s) in training dataset, {testing_set_files_count} files(s) in testing dataset, ({comment_count} comments searched)')
             sys.stdout.write("\033[F")  # Cursor up one line
-            for keyword in keyword_set:
-                if keyword in comment.body:
+            matched = False
+            for pset_set in keyword_search_set:
+                if matched:
+                    continue
+                if all(x in comment.body for x in pset_set):
+                    matched = True
                     print(
                         "-----------------------------------------------------COMMENT-----------------------------------------------------")
+                    print(
+                        f'keyword(s) present: {", ".join(pset_set)}', end="\n\n")
                     highlighted_comment = highlight_keywords(
-                        comment.body, keyword_set)
+                        comment.body, pset_set)
                     print(highlighted_comment, end="\n\n")
                     print(
                         f'link: https://www.reddit.com{comment.permalink}', end="\n\n")
@@ -41,6 +65,12 @@ def collector():
                     if response.lower() == 'y':
                         create_data_file(comment.body, comment)
             comment_count += 1
+
+
+def powerset(iterable):
+    "generate and return powerset of list excluding empty set."
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(1, len(s)+1))
 
 
 def create_data_file(content, file_name):
@@ -76,7 +106,7 @@ def highlight_keywords(comment, keywords):
     # TODO Consider portable way to highlight text via package
     text = comment
     for word in keywords:
-        text = comment.replace(word, f'\033[31m{word}\033[0m')
+        text = text.replace(word, f'\033[31m{word}\033[0m')
     return text
 
 
