c67d86c310aaa57e962e02233433563e4de49b27
Roy Moran <romoran1@outlook.com>
2020-12-14 18:00:40 -0600

    correct utility method to resolve path

--- src/utility.py
+++ src/utility.py
@@ -6,6 +6,6 @@ def resolve_data_directory(dir_indicator) -> str:
             return 'training_set/invalid'
     elif 'testing_set' in dir_indicator:
         if '_valid' in dir_indicator:
-            return 'training_set/valid'
+            return 'testing_set/valid'
         elif '_invalid' in dir_indicator:
-            return 'training_set/invalid'
+            return 'testing_set/invalid'
