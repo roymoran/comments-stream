1322a04b40277400481674da3f05ad1fb1828efa
Roy Moran <romoran1@outlook.com>
2021-01-02 18:58:23 -0600

    remove comment count from stream service

--- src/main.py
+++ src/main.py
@@ -7,12 +7,9 @@ redis_client = redis.Redis(host='queue', port=6379, db=0)
 
 def main():
     stream = stream_generator()
-    comment_count = 0
     for comment in stream:
-        comment_count += 1
         redis_client.rpush("queue:comments", json.dumps(
             {'id': comment.id, 'body': comment.body, 'link': f'https://reddit.com{comment.permalink}'}))
-        print(comment_count)
 
 
 main()
